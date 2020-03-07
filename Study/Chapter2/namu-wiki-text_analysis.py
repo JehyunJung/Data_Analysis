5fa87123ac816c808937e9c82b99dd5b7a44be16
jehyunJung <toojey7@gmail.com>
2020-03-07 16:01:25 +0900

    study: association analysis(namu-wiki,twitter) #200307

---
+++ Study/Chapter2/namu-wiki-text_analysis.py
@@ -0,0 +1,160 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup
+import re
+import warnings
+from konlpy.tag import Okt
+from collections import Counter
+import random
+import pytagcloud
+import webbrowser
+
+
+def text_cleaning(text):
+    hangul = re.compile('[^ ㄱ-ㅣ가-힣]+') # 한글의 정규표현식을 나타냅니다.
+    result = hangul.sub('', text)
+    return result
+def main():
+    # 크롤링할 사이트 주소를 정의합니다.
+    source_url = "https://namu.wiki/RecentChanges"
+
+    # 사이트의 html 구조에 기반하여 크롤링을 수행합니다.
+    req = requests.get(source_url)
+    html = req.text
+    soup = BeautifulSoup(html, 'lxml')
+    contents_table = soup.find(name="table")
+    table_body = contents_table.find(name="tbody")
+    table_rows = table_body.find_all(name="tr")
+
+    warnings.filterwarnings("ignore")
+    # a태그의 href 속성을 리스트로 추출하여, 크롤링 할 페이지 리스트를 생성합니다.
+    page_url_base = "https://namu.wiki"
+    page_urls = []
+    for index in range(0, len(table_rows)):
+        first_td = table_rows[index].find_all('td')[0]
+        td_url = first_td.find_all('a')
+        if len(td_url) > 0:
+            page_url = page_url_base + td_url[0].get('href')
+            if 'png' not in page_url:
+                page_urls.append(page_url)
+
+    # 중복 url을 제거합니다.
+    page_urls = list(set(page_urls))
+    req = requests.get(page_urls[0])
+    html = req.content
+    soup = BeautifulSoup(html, 'lxml')
+    contents_table = soup.find(name="article")
+    title = contents_table.find_all('h1')[0]
+    category = contents_table.find_all('ul')[0]
+    content_paragraphs = contents_table.find_all(name="div", attrs={"class":"wiki-paragraph"})
+    content_corpus_list = []
+    for paragraphs in content_paragraphs:
+        content_corpus_list.append(paragraphs.text)
+    content_corpus = "".join(content_corpus_list)
+    columns = ['title', 'category', 'content_text']
+    df = pd.DataFrame(columns=columns)
+
+    # 각 페이지별 '제목', '카테고리', '본문' 정보를 데이터 프레임으로 만듭니다.
+    for page_url in page_urls:
+
+        # 사이트의 html 구조에 기반하여 크롤링을 수행합니다.
+        req = requests.get(page_url)
+        html = req.content
+        soup = BeautifulSoup(html, 'lxml')
+        contents_table = soup.find(name="article")
+        title = contents_table.find_all('h1')[0]
+        category = contents_table.find_all('ul')[0]
+        content_paragraphs = contents_table.find_all(name="div", attrs={"class":"wiki-paragraph"})
+        content_corpus_list = []
+
+        # 페이지 내 제목 정보에서 개행 문자를 제거한 뒤 추출합니다. 만약 없는 경우, 빈 문자열로 대체합니다.
+        if title is not None:
+            row_title = title.text.replace("\n", " ")
+        else:
+            row_title = ""
+
+        # 페이지 내 본문 정보에서 개행 문자를 제거한 뒤 추출합니다. 만약 없는 경우, 빈 문자열로 대체합니다.
+        if content_paragraphs is not None:
+            for paragraphs in content_paragraphs:
+                if paragraphs is not None:
+                    content_corpus_list.append(paragraphs.text.replace("\n", " "))
+                else:
+                    content_corpus_list.append("")
+        else:
+            content_corpus_list.append("")
+
+        # 페이지 내 카테고리정보에서 “분류”라는 단어와 개행 문자를 제거한 뒤 추출합니다. 만약 없는 경우, 빈 문자열로 대체합니다.
+        if category is not None:
+            row_category = category.text.replace("\n", " ")
+        else:
+            row_category = ""
+
+        # 모든 정보를 하나의 데이터 프레임에 저장합니다.
+        row = [row_title, row_category, "".join(content_corpus_list)]
+        series = pd.Series(row, index=df.columns)
+        df = df.append(series, ignore_index=True)
+    # 텍스트 정제 함수 : 한글 이외의 문자는 전부 제거합니다.
+
+    df['title'] = df['title'].apply(lambda x: text_cleaning(x))
+    df['category'] = df['category'].apply(lambda x: text_cleaning(x))
+    df['content_text'] = df['content_text'].apply(lambda x: text_cleaning(x))
+    title_corpus = "".join(df['title'].tolist())
+    category_corpus = "".join(df['category'].tolist())
+    content_corpus = "".join(df['content_text'].tolist())
+    nouns_tagger = Okt()
+    nouns = nouns_tagger.nouns(content_corpus)
+    count = Counter(nouns)
+    remove_char_counter = Counter({x : count[x] for x in count if len(x) > 1})
+    korean_stopwords_path = "../data/korean_stopwords.txt"
+
+    # 텍스트 파일을 오픈합니다.
+    with open(korean_stopwords_path, encoding='utf8') as f:
+        stopwords = f.readlines()
+    stopwords = [x.strip() for x in stopwords]
+    # 나무위키 페이지에 맞는 불용어를 추가합니다.
+    namu_wiki_stopwords = ['상위', '문서', '내용', '누설', '아래', '해당', '설명', '표기', '추가', '모든', '사용', '매우', '가장',
+                           '줄거리', '요소', '상황', '편집', '틀', '경우', '때문', '모습', '정도', '이후', '사실', '생각', '인물',
+                           '이름', '년월']
+    for stopword in namu_wiki_stopwords:
+        stopwords.append(stopword)
+    remove_char_counter = Counter({x : remove_char_counter[x] for x in count if x not in stopwords})
+
+
+    # 가장 출현 빈도수가 높은 40개의 단어를 선정합니다.
+    ranked_tags = remove_char_counter.most_common(40)
+
+    # pytagcloud로 출력할 40개의 단어를 입력합니다. 단어 출력의 최대 크기는 80으로 제한합니다.
+    taglist = pytagcloud.make_tags(ranked_tags, maxsize=80)
+
+    # pytagcloud 이미지를 생성합니다. 폰트는 나눔 고딕을 사용합니다.
+    pytagcloud.create_tag_image(taglist, 'wordcloud.jpg', size=(900, 600), fontname='NanumGothic', rectangular=False)
+
+    nouns_tagger = Okt()
+    nouns = nouns_tagger.nouns(title_corpus)
+    count = Counter(nouns)
+
+    remove_char_counter = Counter({x : count[x] for x in count if len(x) > 1})
+    remove_char_counter = Counter({x : remove_char_counter[x] for x in count if x not in stopwords})
+
+    ranked_tags = remove_char_counter.most_common(40)
+    taglist = pytagcloud.make_tags(ranked_tags, maxsize=80)
+    pytagcloud.create_tag_image(taglist, 'title_wordcloud.jpg', size=(900, 600), fontname='NanumGothic', rectangular=False)
+
+
+
+    nouns_tagger = Okt()
+    nouns = nouns_tagger.nouns(category_corpus)
+    count = Counter(nouns)
+
+    remove_char_counter = Counter({x : count[x] for x in count if len(x) > 1})
+    remove_char_counter = Counter({x : remove_char_counter[x] for x in count if x not in stopwords})
+
+    ranked_tags = remove_char_counter.most_common(40)
+    taglist = pytagcloud.make_tags(ranked_tags, maxsize=80)
+    pytagcloud.create_tag_image(taglist, 'category_wordcloud.jpg', size=(900, 600), fontname='NanumGothic', rectangular=False)
+
+if __name__ =="__main__":
+    main()
+
